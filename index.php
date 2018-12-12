35427bafb0f6db1605414b804bb9c22bb1a3e1ed
Myrthe van Eijk <Myrthe@145.137.EARLY-REGISTRATION.of.SURFnet.invalid>
2018-12-12 13:28:07 +0100

    Front-end Hotel De Gravin

---
+++ index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+
+    <title>Hotel De Gravin</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <div id="wpr-nav">
+        <div id="cnr-nav">
+            <img id="logo" src="img/HotelDeGravin-logo.jpg" alt="logo" />
+            <div class="admin">
+                <img class="admin-icon" src="img/admin-icon.png" alt="admin" />
+                <p class="admin-style">Myrthe van Eijk</p>
+                <a href="#"><i class="fas fa-sort-down"></i></a>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="wpr-header">
+        <div id="cnr-header">
+        </div>
+    </div>
+
+    <div id="wpr-intro" class="standard-block-style">
+        <div id="cnr-intro">
+            <div class="intro-title">Wanneer wil jij werken?<br>Plan het in!</div>
+            <div class="intro-text">
+                <p>
+                    Is de planning niet helemaal duidelijk.
+                    Of ben je ingepland, maar kun je niet. Geef dit zo snel mogelijk
+                    door via WhatsApp, telefonisch of kom even langs. Hoe eerder wij
+                    het weten, hoe eerder we het kunnen oplossen!
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div id="wpr-available">
+        <div id="cnr-available">
+            <div id="available-info">
+                <img src="img/HotelDeGravin-painting.jpg" alt="Meisje met de parel" />
+                <div id="available-info-block">
+                    <h3>Belangrijke info,<br>regels & tips</h3>
+                    <hr>
+                    <p>
+                        Hotel De Gravin werkt met een weekplanning. Het is belangrijk
+                        dat je op tijd aan geeft wanneer je beschikbaar bent. Om te
+                        voorkomen dat je dit vergeet krijg je elke donderdagochtend een
+                        herinneringsmail.<br>
+                    </p>
+                    <p>
+                        Eenmaal alles ingevuld te hebben moet je niet
+                        vergeten om op versturen te klikken.<br>
+                    </p>
+                    <p>
+                        Opmerkingen is optioneel.
+                        Ook wanneer dit vak leeg is kun je jouw beschikbaarheid versturen.
+                        Daarnaast is het wel handig en fijn als je bijvoorbeeld eerder weg
+                        moet vanwege school, tandarts, etc. je dit ook aangeeft. Zodat wij hier
+                        ook van op de hoogte zijn.
+                    </p>
+                </div>
+            </div>
+            <div id="available-content">
+                <h3>
+                    Selecteer het dagdeel dat je<br>
+                    beschikbaar bent in week 51:
+                </h3>
+
+                <form action="" method="post" >
+                    <div id="monday" class="day">
+                        <p class="day-style">MA</p> <p>19 . dec . 2018</p>
+                        <label><input type="checkbox" id="monday-morning" name="monday-morning" />Ochtend</label>
+                        <label><input type="checkbox" id="monday-afternoon" name="monday-afternoon" />Middag</label>
+                        <label><input type="checkbox" id="monday-evening" name="monday-evening" />Avond</label>
+                    </div>
+
+                    <div id="dinsdag" class="day">
+                        <p class="day-style">DI</p> <p>20 . dec . 2018</p>
+                        <label><input type="checkbox" id="thursday-morning" name="thursday-morning" />Ochtend</label>
+                        <label><input type="checkbox" id="thursday-afternoon" name="thursday-afternoon" />Middag</label>
+                        <label><input type="checkbox" id="thursday-evening" name="thursday-evening" />Avond</label>
+                    </div>
+
+                    <div id="woensdag" class="day">
+                        <p class="day-style">WO</p> <p>21 . dec . 2018</p>
+                        <label><input type="checkbox" id="wednesday-morning" name="wednesday-morning" />Ochtend</label>
+                        <label><input type="checkbox" id="wednesday-afternoon" name="wednesday-afternoon" />Middag</label>
+                        <label><input type="checkbox" id="wednesday-evening" name="wednesday-evening" />Avond</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div id="wpr-contact" class="standard-block-style">
+        <div id="cnr-contact">
+            <div class="intro-title">Vragen en/of<br>opmerkingen?</div>
+            <div class="intro-text">
+                <p>
+                    Welke dag en welk dagdeel ben jij beschikbaar? Selecteer
+                    het dagdeel dat jij beschikbaar bent aan, vul (eventueel) een opmerking in
+                    en verstuur. Wij ontvangen jou beschikbaarheid en maken hier vanuit een planning.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div id="wpr-footer">
+        <div id="cnr-footer">
+            <img id="footer-logo" src="img/HotelDeGravin-logo.jpg" alt="logo" />
+            <div id="footer-owner"><p>Judith & Marianne</p></div>
+        </div>
+    </div>
+
+    <div id="wpr-footer-end">
+        <div id="cnr-footer-end">
+            <p id="disclaimer">Disclaimer</p>
+        </div>
+    </div>
+
+</body>
+</html>
